c555010d32525b29527c024aa9c596aa346052ce
mateuszmarzec <mateuszmikolajmarzec@gmail.com>
2018-07-14 23:10:02 +0200

    FIxes settings.

--- MySite/settings/base.py
+++ MySite/settings/base.py
@@ -131,6 +131,6 @@ STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_HOST_USER = 'help.triplem@gmail.com'
-EMAIL_HOST_PASSWORD = 'triplemhelp'
+EMAIL_HOST_PASSWORD = os.environ.get('EMAIL_PASSWORD'),
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
